7d0458c63defab81c5638613b876024da4e40c85
Matheus Reis <matheusjosereis99@gmail.com>
2021-02-09 16:31:44 -0300

    update final version

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function confirmLink(){
     let shortenedLink = document.getElementById("shorterLink")
     let link = document.getElementById("linkBox").value
+    let element = document.querySelector(".loading")
+
+    shortenedLink.innerHTML = ''
+
+    changeColorLoading(element, `white`)
+
     if(link.length === 0){
+        sleep(2)
         shortenedLink.innerHTML = `Unknow URL`
+        changeColorLoading(element, `#6D6A92`)
         return 
     }
     $.getJSON(
@@ -15,11 +23,28 @@ function confirmLink(){
         function(data){
             let newLink = data.shorturl
             if(newLink === undefined){
+                sleep(2)
                 shortenedLink.innerHTML = `Unknow URL`
+                changeColorLoading(element, `#6D6A92`)
                 return                 
+            }else{
+                sleep(2)
+                shortenedLink.innerHTML = `${newLink}`
+                changeColorLoading(element, `#6D6A92`)
             }
-            shortenedLink.innerHTML = `${newLink}`
-            return shortenedLink
         }
     )
+}
+
+function changeColorLoading(element, color){
+    element.style.borderTopColor = color
+}
+
+function sleep(seconds) {
+    let milisseconds = seconds*1000
+    const date = Date.now()
+    let currentDate
+    do{
+        currentDate = Date.now()
+    }while(currentDate - date < milisseconds)
 }
